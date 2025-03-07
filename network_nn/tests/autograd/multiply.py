f9716e252be9c33782e69924869b7c28ce6ae810
Coder1010ayush <coderayush1010@gmail.com>
2025-03-07 14:18:05 +0530

    element wise multiplicatiion test cases are added

--- network_nn/tests/autograd/multiply.py
+++ network_nn/tests/autograd/multiply.py
@@ -1,41 +1,41 @@
-# ----------------------------- utf-8 encoding --------------------------
-# this file contains how pytorch calculate grad for addition for same shape tensor and also broadcastable shape
+# ---------------------------------------------- utf-8 encoding ------------------------------------
+# this file contains the test cases for elementwise multiplication
 import torch
 
 
 def type_a():
-    A = torch.randn(3, 4, dtype=torch.float32, requires_grad=True)
-    B = torch.randn(3, 4, dtype=torch.float32, requires_grad=True)
+    A = torch.randn(2, 4, dtype=torch.float32, requires_grad=True)
+    B = torch.randn(2, 4, dtype=torch.float32, requires_grad=True)
     return A, B
 
 
 def type_b():
-    A = torch.randn(3, 1, dtype=torch.float32, requires_grad=True)
-    B = torch.randn(3, 4, dtype=torch.float32, requires_grad=True)
+    A = torch.randn(2, 1, dtype=torch.float32, requires_grad=True)
+    B = torch.randn(2, 4, dtype=torch.float32, requires_grad=True)
     return A, B
 
 
 def type_c():
-    A = torch.randn(3, 4, dtype=torch.float32, requires_grad=True)
+    A = torch.randn(2, 4, dtype=torch.float32, requires_grad=True)
     B = torch.randn(4, dtype=torch.float32, requires_grad=True)
     return A, B
 
 
 def type_d():
-    A = torch.randn(3, 4, dtype=torch.float32, requires_grad=True)
+    A = torch.randn(2, 4, dtype=torch.float32, requires_grad=True)
     B = torch.tensor(2.0, dtype=torch.float32, requires_grad=True)
     return A, B
 
 
 def type_e():
-    A = torch.randn(1, 3, 4, dtype=torch.float32, requires_grad=True)
-    B = torch.randn(3, 4, dtype=torch.float32, requires_grad=True)
+    A = torch.randn(1, 2, 4, dtype=torch.float32, requires_grad=True)
+    B = torch.randn(2, 4, dtype=torch.float32, requires_grad=True)
     return A, B
 
 
 def type_f():
-    A = torch.randn(2, 3, 4, dtype=torch.float32, requires_grad=True)
-    B = torch.randn(3, 4, dtype=torch.float32, requires_grad=True)
+    A = torch.randn(2, 2, 4, dtype=torch.float32, requires_grad=True)
+    B = torch.randn(2, 4, dtype=torch.float32, requires_grad=True)
     return A, B
 
 
@@ -44,7 +44,7 @@ def compute_gradient():
         [type_a, type_b, type_c, type_d, type_e, type_f], start=1
     ):
         a, b = tensor_func()
-        z = a + b
+        z = a * b
         z.retain_grad()
         out = z.sum()
         out.retain_grad()
