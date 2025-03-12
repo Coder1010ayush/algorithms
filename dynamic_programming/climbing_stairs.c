557aa83d9fc96eac4de322808e878532dc3b1c08
Coder1010ayush <coderayush1010@gmail.com>
2025-03-13 00:12:02 +0530

    generalizing climbing staire problem

--- dynamic_programming/climbing_stairs.c
+++ dynamic_programming/climbing_stairs.c
@@ -1,3 +1,4 @@
+// --------------------------------------------------- utf-8 encoding ----------------------------------------
 /*
 
     Climbing Stairs Problem :
@@ -33,6 +34,45 @@ int calculate_number_ways_climb_stairs_memo(int n)
     memo[n] = calculate_number_ways_climb_stairs_memo(n - 1) + calculate_number_ways_climb_stairs_memo(n - 2);
     return memo[n];
 }
+
+/*
+    let us generalize this problem instead of taking 1 or 2 steps , steps will be taken from  a list or set given.
+*/
+int calculate_number_ways_climb_stairs_general(int n, int steps[], int step_count)
+{
+
+    if (n == 0)
+        return 1;
+    if (n < 0)
+        return 0;
+
+    int t = 0;
+    for (int i = 0; i < step_count; i++)
+    {
+        t += calculate_number_ways_climb_stairs_general(n - steps[i], steps, step_count);
+    }
+    return t;
+}
+
+int calculate_number_ways_climb_stairs_general_memo(int n, int steps[], int step_count)
+{
+
+    if (n == 0)
+        return 1;
+    if (n < 0)
+        return 0;
+    if (memo[n] != -1)
+        return memo[n];
+
+    int t = 0;
+    for (int i = 0; i < step_count; i++)
+    {
+        t += calculate_number_ways_climb_stairs_general(n - steps[i], steps, step_count);
+    }
+    memo[n] = t;
+    return memo[n];
+}
+
 int main()
 {
     for (int i = 0; i < MAX; i++)
@@ -40,7 +80,10 @@ int main()
         memo[i] = -1;
     }
     int number_of_stairs = 4;
+    int steps[2] = {1, 2};
+    int step_count = 2;
     // int res = calculate_number_ways_climb_stairs(4);
-    int res = calculate_number_ways_climb_stairs_memo(4);
+    // int res = calculate_number_ways_climb_stairs_memo(4);
+    int res = calculate_number_ways_climb_stairs_general(number_of_stairs, steps, step_count);
     printf("Total number of ways is %d ", res);
 }
